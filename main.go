ca43cce31f10607e7de8be645ab271de17a2ad36
Andrei <zaharof.andrey@yandex.ru>
2025-06-28 13:29:52 +0500

    Добавлен пример композиции

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	"net/url"
 )
 
 type account struct {
@@ -11,21 +13,46 @@ type account struct {
 	url      string
 }
 
+func (acc *account) outputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(res)
+}
+
+func newAccount(login, password, urlString string) (*account, error) {
+
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("Invalid URL")
+	}
+
+	return &account{
+		url:      urlString,
+		login:    login,
+		password: password,
+	}, nil
+}
+
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
 
 func main() {
-	fmt.Println(generatePassword(12))
 	login := promptData("Введите логин")
 	password := promptData("Введите пароль")
 	url := promptData("Введите URL")
-
-	myAccount := account{
-		password: password,
-		url:      url,
-		login:    login,
+	myAccount, err := newAccount(login, password, url)
+	if err != nil {
+		fmt.Println("Неверный формат URL")
+		return
 	}
-
-	outputPassword(&myAccount)
+	myAccount.generatePassword(12)
+	myAccount.outputPassword()
+	fmt.Println(myAccount)
 }
 
 func promptData(prompt string) string {
@@ -33,17 +60,4 @@ func promptData(prompt string) string {
 	var res string
 	fmt.Scan(&res)
 	return res
-}
-
-func outputPassword(acc *account) {
-	fmt.Println(acc)
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func generatePassword(n int) string {
-	res := make([]rune, n)
-	for i := range res {
-		res[i] = letterRunes[rand.IntN(len(letterRunes))]
-	}
-	return string(res)
 }
